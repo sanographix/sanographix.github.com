699ab128c11cf14b4cff57847192de7fe27f5b83
sanographix <mail@sanographix.net>
2012-11-28 00:08:34 +0900

    テキスト最初非表示に

--- js/jquery.options.js
+++ js/jquery.options.js
@@ -6,18 +6,10 @@ $(function(){
 
   setInterval(function(){
       $(".overlay")
-      .animate({ backgroundColor: "#ff0000"}, 400) //赤
-      .animate({ backgroundColor: "#de00ff"}, 400) //紫
-      .animate({ backgroundColor: "#0018ff"}, 400) //青
-      .animate({ backgroundColor: "#00e4ff"}, 400) //水
-      .animate({ backgroundColor: "#00ff2a"}, 400) //緑
-      .animate({ backgroundColor: "#ff8a00"}, 400) //オレンジ
-
-
-
-
-
-
-  }, 2400);
+      .animate({ backgroundColor: "#ff0000"}, 440) //赤
+      .animate({ backgroundColor: "#de00ff"}, 440) //紫
+      .animate({ backgroundColor: "#00aeff"}, 440) //青
+      .animate({ backgroundColor: "#00ff2a"}, 440) //緑
+  }, 0);
 
 });
