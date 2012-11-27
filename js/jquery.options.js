07477f66033bec062405bf87284dc2c11cab9b8a
sanographix <mail@sanographix.net>
2012-11-27 19:35:17 +0900

    背景色が点滅する

--- js/jquery.options.js
+++ js/jquery.options.js
@@ -4,4 +4,20 @@ $(function(){
     $(".container").fadeToggle("slow");
   });
 
+  setInterval(function(){
+      $(".overlay")
+      .animate({ backgroundColor: "#ff0000"}, 400) //赤
+      .animate({ backgroundColor: "#de00ff"}, 400) //紫
+      .animate({ backgroundColor: "#0018ff"}, 400) //青
+      .animate({ backgroundColor: "#00e4ff"}, 400) //水
+      .animate({ backgroundColor: "#00ff2a"}, 400) //緑
+      .animate({ backgroundColor: "#ff8a00"}, 400) //オレンジ
+
+
+
+
+
+
+  }, 2400);
+
 });
