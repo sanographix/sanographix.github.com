1e1a76db6fc4b16fad6dac908ebe979e95c01af9
sanographix <mail@sanographix.net>
2012-12-25 12:00:25 +0900

    カーニングやめる

--- public/tumblr/memographix/js/options.js
+++ public/tumblr/memographix/js/options.js
@@ -1,6 +1,5 @@
 $(function() {
   setTimeout(function(){
-    $("body").addClass('load');
     
     $('.hatena-star-add-button').tipsy({
     gravity: 's',
@@ -10,13 +9,3 @@ $(function() {
     $('.hatena-star-add-button').attr("title","★をつける");
   },1200);
 });
-function init()
-{
-  FLAutoKerning.process($('header h1 a'), FLAutoKerning.DEFAULT_KERNING_INFO);
-  FLAutoKerning.process($('.entry-content h1'), FLAutoKerning.DEFAULT_KERNING_INFO);
-  FLAutoKerning.process($('.entry-content h2'), FLAutoKerning.DEFAULT_KERNING_INFO);
-  FLAutoKerning.process($('.entry-content h3'), FLAutoKerning.DEFAULT_KERNING_INFO);
-  FLAutoKerning.process($('.entry-content h4'), FLAutoKerning.DEFAULT_KERNING_INFO);
-  FLAutoKerning.process($('.entry-content h5'), FLAutoKerning.DEFAULT_KERNING_INFO);
-  FLAutoKerning.process($('.entry-content h6'), FLAutoKerning.DEFAULT_KERNING_INFO);
-}
