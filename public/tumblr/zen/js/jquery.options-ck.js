b55f22d55c31c30ff835601043b3e4c66abee871
sanographix <mail@sanographix.net>
2013-02-22 23:58:36 +0900

    zenテーマのcss

--- public/tumblr/zen/js/jquery.options-ck.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$("#youtube-iframe").fitVids();$(".mobile-nav-panel").click(function(){$(".nav").toggleClass("active")});$(".panel-social-button").click(function(){$(".social-buttons").slideToggle()})});
--- public/tumblr/zen/js/jquery.options.js
+++ public/tumblr/zen/js/jquery.options.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
     $("#youtube-iframe").fitVids();
 
-    $(".mobile-nav-panel").click(function(){
+    $(".mobile-nav-panel i").click(function(){
       $(".nav").toggleClass("active");
     });
 
-    $(".panel-social-button").click(function(){
+    $(".panel-social-button .btn").click(function(){
       $(".social-buttons").slideToggle();
     });
+
+  setTimeout(function(){
+
+    $('.hatena-star-add-button').tipsy({
+    gravity: 's',
+    fade: false
+    });
+
+    $('.hatena-star-add-button').attr("title","★をつける");
+  },3000);
 
 });
