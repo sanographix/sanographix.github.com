485c871a61dac67b1aeeffe50d5d9fddcea32375
sanographix <mail@sanographix.net>
2013-03-04 18:50:17 +0900

    メディアクエリ

--- public/tumblr/illustfolio/js/jquery.options.js
+++ public/tumblr/illustfolio/js/jquery.options.js
@@ -13,6 +13,29 @@ $(function(){
   $(window).load(function(){
 
     // photoset(979px以下)
+    if ($(window).width() < 1200) {
+
+      $('iframe.photoset').contents().find('.photoset_row').css({
+        width : 'auto',
+        height : 'auto'
+      });
+
+      $('iframe.photoset').contents().find('.photoset_photo').css({
+        display : 'block',
+        margin : '0 0 0 0'
+      });
+
+      $('iframe.photoset').contents().find('.photoset_row img').css({
+        width : '500px',
+        margin : '0 0 0 0'
+      });
+
+      $('iframe.photoset').css('height', $('iframe.photoset').contents().find('body').height() + 'px');
+
+    }
+
+
+    // photoset(979px以下)
     if ($(window).width() < 979) {
 
       $('iframe.photoset').contents().find('.photoset_row').css({
