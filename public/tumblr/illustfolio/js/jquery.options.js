848c54d0060df59ad2cd90d06c05d65e4baa8732
sanographix <mail@sanographix.net>
2013-03-04 18:47:40 +0900

    photosetのメディアクエリ

--- public/tumblr/illustfolio/js/jquery.options.js
+++ public/tumblr/illustfolio/js/jquery.options.js
@@ -9,4 +9,57 @@ $(function(){
       slideshowSpeed: 6000
     });
 
+
+  $(window).load(function(){
+
+    // photoset(979px以下)
+    if ($(window).width() < 979) {
+
+      $('iframe.photoset').contents().find('.photoset_row').css({
+        width : 'auto',
+        height : 'auto'
+      });
+
+      $('iframe.photoset').contents().find('.photoset_photo').css({
+        display : 'block',
+        margin : '0 0 0 0'
+      });
+
+      $('iframe.photoset').contents().find('.photoset_row img').css({
+        width : '400px',
+        margin : '0 0 0 0'
+      });
+
+      $('iframe.photoset').css('height', $('iframe.photoset').contents().find('body').height() + 'px');
+
+    }
+
+
+    // スマホでphotosetの中をwidth100%にする
+    if ($(window).width() < 480) {
+
+      $('iframe.photoset').contents().find('.photoset_row').css({
+        width : 'auto',
+        height : 'auto'
+      });
+
+      $('iframe.photoset').contents().find('.photoset_photo').css({
+        display : 'block',
+        margin : '0 0 0 0'
+      });
+
+      $('iframe.photoset').contents().find('.photoset_row img').css({
+        width : '300px',
+        margin : '0 0 0 0'
+      });
+
+      $('iframe.photoset').css('height', $('iframe.photoset').contents().find('body').height() + 'px');
+
+    }
+
+
+
+  });
+
+
 });
