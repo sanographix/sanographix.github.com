1c96b7a6413f0cc88d5c0e05a2fdcc40f3bf4054
sanographix <mail@sanographix.net>
2014-02-22 01:42:39 +0900

    memographix2

---
+++ public/tumblr/memographix2/js/memographix-js.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $(".video-iframe").fitVids();
+
+    $(".mobile-nav-panel").click(function(){
+      $(".nav").toggleClass("active");
+    });
+
+    // シンタックスハイライト用のclassつける
+    $(".entry-content pre:not(.line-pre)").addClass("prettyprint"); // gistは除外
+
+  setTimeout(function(){
+
+    $('.hatena-star-add-button').tipsy({
+    gravity: 's',
+    fade: false
+    });
+
+    $('.hatena-star-add-button').attr("title","★をつける");
+  },3000);
+
+
+  $(window).load(function(){
+
+    // スマホでphotosetの中をwidth100%にする
+    if ($(window).width() < 480) {
+
+      $('iframe.photoset').contents().find('.photoset_row').css({
+        width : 'auto',
+        height : 'auto'
+      });
+
+      $('iframe.photoset').contents().find('.photoset_photo').css({
+        display : 'block',
+        margin : '0 0 0 0'
+      });
+
+      $('iframe.photoset').contents().find('.photoset_row img').css({
+        width : '300px',
+        margin : '0 0 0 0'
+      });
+
+      $('iframe.photoset').css('height', $('iframe.photoset').contents().find('body').height() + 'px');
+
+    }
+
+  });
+
+
+});
