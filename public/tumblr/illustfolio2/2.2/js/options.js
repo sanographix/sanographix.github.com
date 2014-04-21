15bec6e31ab15658f234d582c7dacce7c0ff0114
sanographix <mail@sanographix.net>
2014-04-22 00:19:56 +0900

    フォトセットの挙動を元に戻す

--- public/tumblr/illustfolio2/2.2/js/options.js
+++ public/tumblr/illustfolio2/2.2/js/options.js
@@ -49,5 +49,24 @@ $(function () {
   // fitvids
   $(".video-iframe").fitVids();
 
+  // スマホとタブレットでphotosetの中をwidth100%にする
+  $(window).load(function(){
+    if ($(window).width() < 767) {
+      $('iframe.photoset').contents().find('.photoset_row').css({
+        width : 'auto',
+        height : 'auto'
+      });
+      $('iframe.photoset').contents().find('.photoset_photo').css({
+        display : 'block',
+        margin : '0 0 0 0'
+      });
+      $('iframe.photoset').contents().find('.photoset_row img').css({
+        width : '100%',
+        margin : '0 0 0 0'
+      });
+      $('iframe.photoset').css('height', $('iframe.photoset').contents().find('body').height() + 'px');
+    }
+  });
+
 
 });
