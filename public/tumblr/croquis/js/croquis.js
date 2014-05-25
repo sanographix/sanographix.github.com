98193dd169ab7aff1f67fc81750f332c7b1dec11
sanographix <mail@sanographix.net>
2014-05-25 23:57:18 +0900

    croquisのフォトセットも修正

--- public/tumblr/croquis/js/croquis.js
+++ public/tumblr/croquis/js/croquis.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
     $(".video-iframe").fitVids();
+
+    $(window).load(function(){
+
+      // photoset width100% when browser size is under 540px
+      if ($(window).width() < 540) {
+
+        $('iframe.photoset').contents().find('.photoset_row').css({
+          width : 'auto',
+          height : 'auto',
+          margin : '0'
+        });
+
+        $('iframe.photoset').contents().find('.photoset_photo').css({
+          display : 'block',
+          margin : '0 0 0 0'
+        });
+
+        $('iframe.photoset').contents().find('.photoset_row img').css({
+          width : '100%',
+          margin : '0 0 0 0'
+        });
+
+        $('iframe.photoset').css('height', $('iframe.photoset').contents().find('body').height() + 'px');
+
+      }
+
+    });
+
 });
