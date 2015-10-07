c688bdb0b8fb260d862f49092d43ef00565b5255
sanographix <mail@sanographix.net>
2015-10-07 09:00:33 +0900

    twitterシェア数取得できなくなったのでコード消す

--- public/tumblr/memo.sanographix.net/js/scripts.js
+++ public/tumblr/memo.sanographix.net/js/scripts.js
@@ -329,54 +329,6 @@ $(document).ready(function(){
 
 });
 
-// Facebookの反応（いいねとシェアの数）を取得
-function get_social_count_facebook(url, counterId) {
-  $.ajax({
-    url:'https://graph.facebook.com/',
-    dataType:'jsonp',
-    data:{
-      id:url
-    },
-    success:function(res){
-      $('#' + counterId + ' .count').text( res.shares || 0 );
-    },
-    error:function(){
-      $('#' + counterId + ' .count').text('');
-    }
-  });
-}
-// Twitterの反応（ツイートやリツイート数）を取得
-function get_social_count_twitter(url, counterId) {
-  $.ajax({
-    url:'http://urls.api.twitter.com/1/urls/count.json',
-    dataType:'jsonp',
-    data:{
-      url:url
-    },
-    success:function(res){
-      $('#' + counterId + ' .count').text( res.count || 0 );
-    },
-    error:function(){
-      $('#' + counterId + ' .count').text('');
-    }
-  });
-}
-// はてなブックマークでブックマークされている数を取得
-function get_social_count_hatebu(url, counterId) {
-  $.ajax({
-    url:'http://api.b.st-hatena.com/entry.count?callback=?',
-    dataType:'jsonp',
-    data:{
-      url:url
-    },
-    success:function(res){
-      $('#' + counterId + ' .count').text( res || 0 );
-    },
-    error:function(){
-      $('#' + counterId + ' .count').text('');
-    }
-  });
-}
 
 // ページの最初の画像をogimageとする
 function img_find() {
